e46a00ba37fc83ff876a8ef80ac901067fe4d013
Antti Ajanki <antti.ajanki@iki.fi>
2022-03-10 20:27:04 +0200

    cleanup

--- resources/lib/logger.py
+++ resources/lib/logger.py
@@ -1,25 +1,17 @@
 try:
     import xbmc
-
-    LOGDEBUG = xbmc.LOGDEBUG
-    LOGINFO = xbmc.LOGINFO
-    LOGWARNING = xbmc.LOGWARNING
-    LOGERROR = xbmc.LOGERROR
-
-    # TODO
-    def log(message: str, level: int = LOGDEBUG) -> None:
-        xbmc.log(f'[{_addonid}] {message}', level)
-
 except ImportError:
-    LOGDEBUG = 10
-    LOGINFO = 20
-    LOGWARNING = 30
-    LOGERROR = 30
-
-    def log(message: str, level: int = LOGDEBUG) -> None:
-        print(f'[{_addonid}] {message}')
+    xbmc = None
 
 _addonid = 'plugin.video.areenatest'
+
+
+def log_xbmc(message: str, level: int) -> None:
+    xbmc.log(f'[{_addonid}] {message}', level)
+
+
+def log_print(message: str, level: int) -> None:
+    print(f'[{_addonid}] {message}')
 
 
 def debug(message: str) -> None:
@@ -36,3 +28,22 @@ def warning(message: str) -> None:
 
 def error(message: str) -> None:
     log(message, LOGERROR)
+
+
+if xbmc is None:
+    # xbmc is not available. Use print for logging.
+    # This is useful for testing in development environment. On Kodi, the xbmc
+    # is of course available and we'll go to the else branch.
+    log = log_print
+
+    LOGDEBUG = 10
+    LOGINFO = 20
+    LOGWARNING = 30
+    LOGERROR = 30
+else:
+    log = log_xbmc
+
+    LOGDEBUG = xbmc.LOGDEBUG
+    LOGINFO = xbmc.LOGINFO
+    LOGWARNING = xbmc.LOGWARNING
+    LOGERROR = xbmc.LOGERROR
