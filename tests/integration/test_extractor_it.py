835674e408bbcd619cebd6d9ef6b454fa576f29c
Antti Ajanki <antti.ajanki@iki.fi>
2023-11-22 19:54:57 +0200

    Disable kaltura, because Areena web doesn't use it anymore

--- tests/integration/test_extractor_it.py
+++ tests/integration/test_extractor_it.py
@@ -5,4 +5,4 @@ def test_extract_html5():
     manifest_url = extractor.extract_media_url('yleareena://items/1-787136')
 
     assert manifest_url is not None
-    assert '/manifest.mpd' in manifest_url.url
+    assert '.m3u8' in manifest_url.url
